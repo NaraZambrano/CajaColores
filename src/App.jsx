00620fcffd82e4481aac485ceef3282231a5c74f
NaraZambrano <lourdes.zambrano@ikasle.aeg.eus>
2024-03-22 09:02:22 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const purpleRef = useRef(null);
+  const orangeRef = useRef(null);
+  const yellowRef = useRef(null);
+  const greenRef = useRef(null);
+  const redRef = useRef(null);
+  const cyanRef = useRef(null);
+  const violetRef = useRef(null);
+  const grayRef = useRef(null);
+  const meatRef = useRef(null);
+
+  const colors = [
+    {
+      color:'#D7BDE2',
+      ref: purpleRef
+    },
+    {
+      color:'#FA9503',
+      ref: orangeRef
+    },
+    {
+      color:'#FAF303',
+      ref: yellowRef
+    },
+    {
+      color:'#0AFA03',
+      ref: greenRef
+    },
+    {
+      color:'#FA0E03',
+      ref: redRef
+    },
+    {
+      color:'#FA03E7',
+      ref: violetRef
+    },
+    {
+      color:'#03FAEF',
+      ref: cyanRef
+    },
+    {
+      color:'#CACFD2',
+      ref: grayRef
+    },
+    {
+      color:'#F4D03F',
+      ref: meatRef
+    }
+  ];
+  const [currentColor, setCurrentColor] = useState('#FFFFFF');
+
+  const handleClick = ((index, itemRef) => {
+    itemRef.current.style.scale = (0.2);
+    setTimeout(() => {
+    itemRef.current.style.scale = (1);
+      setCurrentColor(colors[index].color); 
+    }, 500);
+  })
+
+  useEffect(() => {
+    console.log(currentColor);
+  }, [currentColor])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='colors-container'>
+        {
+          colors.map((item, index) => {
+            return <div 
+              key={index} 
+              ref={item.ref}
+              className='color-card' 
+              style={{backgroundColor: item.color}} 
+              onClick={() => handleClick(index,item.ref)}
+              >
+            </div>
+          })
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='colors-container'>
+        {
+          colors.map((color, index) => {
+            return <div key={index} className='color-card' style={{backgroundColor: currentColor}} ></div>
+          })
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
